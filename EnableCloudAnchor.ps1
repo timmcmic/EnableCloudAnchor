39681cc3a57ce415025f975a1ee6257286650a7d
timmcmic <timmcmic@microsoft.com>
2024-10-04 11:25:27 -0500

    Updating

--- EnableCloudAnchor.ps1
+++ EnableCloudAnchor.ps1
@@ -52,6 +52,85 @@ Param(
     [string]$logFolderPath=$NULL
 )
 
+<#
+
+Sample RAW powershell output for creating the contact writeback rule.
+
+New-ADSyncRule  `
+-Name 'Out to AD - Contact Write CloudAnchor' `
+-Identifier '9d41063c-1713-425f-b097-cac31120ac0e' `
+-Description '' `
+-Direction 'Outbound' `
+-Precedence 10 `
+-PrecedenceAfter '00000000-0000-0000-0000-000000000000' `
+-PrecedenceBefore '00000000-0000-0000-0000-000000000000' `
+-SourceObjectType 'person' `
+-TargetObjectType 'contact' `
+-Connector '4f1cdd9e-00fa-4379-be83-4cf471f7c829' `
+-LinkType 'Join' `
+-SoftDeleteExpiryInterval 0 `
+-ImmutableTag '' `
+-OutVariable syncRule
+
+
+Add-ADSyncAttributeFlowMapping  `
+-SynchronizationRule $syncRule[0] `
+-Source @('cloudAnchor') `
+-Destination 'msDS-ExternalDirectoryObjectId' `
+-FlowType 'Direct' `
+-ValueMergeType 'Update' `
+-OutVariable syncRule
+
+
+Add-ADSyncRule  `
+-SynchronizationRule $syncRule[0]
+
+
+Get-ADSyncRule  `
+-Identifier '9d41063c-1713-425f-b097-cac31120ac0e'
+
+#>
+
+<#
+
+Sample RAW powershell output for running authoritative null to revert writeback.
+
+New-ADSyncRule  `
+-Name 'Out to AD - Contact Write CloudAnchor (Revert WriteBack)' `
+-Identifier '31645b42-bde4-4961-980a-d6c677dda74b' `
+-Description '' `
+-Direction 'Outbound' `
+-Precedence 11 `
+-PrecedenceAfter '00000000-0000-0000-0000-000000000000' `
+-PrecedenceBefore '00000000-0000-0000-0000-000000000000' `
+-SourceObjectType 'person' `
+-TargetObjectType 'contact' `
+-Connector '4f1cdd9e-00fa-4379-be83-4cf471f7c829' `
+-LinkType 'Join' `
+-SoftDeleteExpiryInterval 0 `
+-ImmutableTag '' `
+-Disabled  `
+-OutVariable syncRule
+
+
+Add-ADSyncAttributeFlowMapping  `
+-SynchronizationRule $syncRule[0] `
+-Destination 'msDS-ExternalDirectoryObjectId' `
+-FlowType 'Expression' `
+-ValueMergeType 'Update' `
+-Expression 'AuthoritativeNull' `
+-OutVariable syncRule
+
+
+Add-ADSyncRule  `
+-SynchronizationRule $syncRule[0]
+
+
+Get-ADSyncRule  `
+-Identifier '31645b42-bde4-4961-980a-d6c677dda74b'
+
+#>
+
 $ErrorActionPreference = 'Stop'
 
 #*****************************************************
